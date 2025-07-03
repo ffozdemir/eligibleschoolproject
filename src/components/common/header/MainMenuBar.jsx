d3d860a46713633ac0e09ae2e3aa46c97fe17bfb
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-07-03 15:07:58 +0200

    Add header components: Logo, MainMenuBar, and Topbar with styles

--- src/components/common/header/MainMenuBar.jsx
+++ src/components/common/header/MainMenuBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Nav, Container, Navbar, Offcanvas } from "react-bootstrap";
+import Logo from "./Logo";
 
 const MainMenuBar = () => {
   return (
-    <div>MainMenuBar</div>
-  )
-}
+    <Navbar expand="lg" className="bg-body-tertiary mb-3" sticky="top">
+      <Container>
+       <Logo/>
+        <Navbar.Toggle aria-controls="main-menubar" />
+        <Navbar.Offcanvas
+          id="main-menubar"
+          aria-labelledby="main-menubar-title"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="main-menubar-title">Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+            </Nav>
+            Hello
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default MainMenuBar
+export default MainMenuBar;
--- src/components/common/header/Topbar.jsx
+++ src/components/common/header/Topbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { appConfig } from "@/helpers/config";
+import React from "react";
+import { Container } from "react-bootstrap";
+import "@/components/common/header/topbar.scss"
+
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <div className="topbar">
+      <Container>
+        <div className="slogan">
+          <i className="pi pi-megaphone"> {appConfig.project.slogan}</i>
+        </div>
+        <div>
+          <i className="pi pi-user"> Login</i>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default Topbar
+export default Topbar;
