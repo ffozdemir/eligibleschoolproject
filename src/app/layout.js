ad7f9dbbf77afe6b17e3ecc55f2899b7fc76cf08
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-07-03 14:37:05 +0200

    Add layout, font, and style variables for project configuration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,19 @@
 import { appConfig } from "@/helpers/config";
+import { montserrat } from "@/helpers/fonts";
+import "@/styles/index.scss";
 
 export const metadata = {
-	title: {
-		template: `%s | ${appConfig.project.name}`,
-		default: appConfig.project.name,
-	},
-	description: appConfig.project.description,
+  title: {
+    template: `%s | ${appConfig.project.name}`,
+    default: appConfig.project.name,
+  },
+  description: appConfig.project.description,
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				
-				{children}
-			
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={montserrat.variable}>
+      <body>{children}</body>
+    </html>
+  );
 }
